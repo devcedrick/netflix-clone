a0efc9585c5ab3062cdbf502d66728985c857985
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-08-07 11:32:58 +0800

    [feat] Add hero section and buttons to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css'
 import Navbar from '../../components/Navbar/Navbar';
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
+import play_icon from '../../assets/play_icon.png'
+import info_icon from '../../assets/info_icon.png'
 
 const Home = () => {
   return (
@@ -11,7 +13,20 @@ const Home = () => {
       <div className="hero">
         <img src={hero_banner} alt="Hero Banner" className='banner-img' />
         <div className="hero-caption">
-          <img src={hero_title} alt="" />
+          <img src={hero_title} alt="Hero title" className='caption-img' />
+          <p> 
+            Discovering his ties to a secret ancient order, a yound man lying in modern Istanbuk embarks on a quest to save the city from an immortal enemy. 
+          </p>
+          <div className="hero-btns">
+            <button className='btn'>
+              <img src={play_icon} alt="" />
+              Play
+            </button>
+            <button className='btn dark-btn'>
+              <img src={info_icon} alt="" />
+              More Info
+            </button>
+          </div>
         </div>
       </div>
     </div>

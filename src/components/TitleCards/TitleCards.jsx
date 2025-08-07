2f226d08922ccaf7c4a21d0d5fb7e44f7653bb82
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-08-07 13:32:05 +0800

    [feat] Implement TitleCards component and integrate into Home page

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
 
-const TitleCards = () => {
+const TitleCards = ({title}) => {
+  // useRef() to access a DOM element
+  const cardRef = useRef();
+
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardRef.current.scrollLeft += event.deltaY;
+  }
+
+  useEffect(() => {
+    cardRef.current.addEventListener('wheel', handleWheel);
+  }, []);
+
   return (
     <div className='title-card'>
-      
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardRef}>
+        {cards_data.map((card, index) => {
+          return <div className="card" key={index}>
+            <img src={card.image} alt={card.name} />
+            <p>{card.name}</p>
+          </div>
+        })};
+      </div>
     </div>
   )
 }
